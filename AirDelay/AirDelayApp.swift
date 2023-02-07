73c69e74d35fb143baa1f9e77bf393579c835073
lukasjankuss <lukasjankuss@gmail.com>
2023-02-07 14:26:07 +0000

    Initial Commit

---
+++ AirDelay/AirDelayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirDelayApp.swift
+//  AirDelay
+//
+//  Created by Lukas Jankus on 07/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct AirDelayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
